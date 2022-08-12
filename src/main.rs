7629b79b9dc421aceb903bcfa5cf73c1745f5d75
Matt Robertson <mattrobertson333@gmail.com>
2022-08-11 19:37:48 -0500

    Tweaked template to only support Arduino Uno

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,17 @@
 #![no_main]
 
 use panic_halt as _;
+use arduino_hal::{Peripherals, pins, delay_ms, entry};
 
-#[arduino_hal::entry]
+#[entry]
 fn main() -> ! {
-    let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::pins!(dp);
+    let dp = Peripherals::take().unwrap();
+    let pins = pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
-    {% case board -%}
-      {%- when "Adafruit Trinket" -%}
-    let mut led = pins.d1.into_output();
-      {%- when "Arduino Leonardo", "Arduino Mega 2560", "Arduino Nano", "Arduino Uno", "Nano168", "Adafruit Trinket Pro" -%}
     let mut led = pins.d13.into_output();
-      {%- when "SparkFun ProMicro" -%}
-    let mut led = pins.led_rx.into_output();
-    {%- endcase %}
 
     loop {
         led.toggle();
-        arduino_hal::delay_ms(1000);
+        delay_ms(1000);
     }
 }
-{%- comment %}
-# vim: ft=rust.jinja2
-{% endcomment %}
